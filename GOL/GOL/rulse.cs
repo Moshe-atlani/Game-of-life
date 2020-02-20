65100fb76167a2f942fcb9022c10d7b6e3f65362
Moshe-atlani <atlani.moshe@gmail.com>
2020-02-20 13:57:01 +0200

    j ai ajouter les cas qui avait un problem

--- GOL/GOL/rulse.cs
+++ GOL/GOL/rulse.cs
@@ -23,8 +23,8 @@ namespace GOL
             {
                 for (int j = 0; j < b.GetLength(1); j++)//tour
                 {
-                    //if (i != 0 && i != b.GetLength(0) && j != 0 && j != b.GetLength(1))//senser vairifier si on ne sort pas de la matrix a cause de + ou - j ou i
-                    //{ 
+                    if (i != 0 && i != b.GetLength(0) && j != 0 && j != b.GetLength(1))//senser vairifier si on ne sort pas de la matrix a cause de + ou - j ou i
+                    { 
                         //j ai ce if psk j avai une pb (Index was outside the bounds of the array.)
                                                                                        //[ ][ ][ ]
                                                                                        //[ ][?][ ]
@@ -70,7 +70,168 @@ namespace GOL
                         //[ ][ ][ ]
                         //[ ][1][ ]
                         //[ ][ ][?]
-                    //}
+                    }
+                    else
+                    {
+                        if(i == 0 && j == b.GetLength(0))
+                        {
+                            if (b[i, j - 1] == '1')
+                                neighbour++;
+                            if (b[i + 1, j - 1] == '1')
+                                neighbour++;
+                            if (b[i + 1, j] == '1')
+                                neighbour++;
+                        }
+
+                        if(i == 0 && j == 0)
+                        {
+                            if (b[i, j + 1] == '1')
+                                neighbour++;
+                            if (b[i + 1, j + 1] == '1')
+                                neighbour++;
+                            if (b[i + 1, j] == '1')
+                                neighbour++;
+                        }
+
+                        if(i == b.GetLength(1) && j == 0)
+                        {
+                            if (b[i - 1, j] == '1')
+                                neighbour++;
+                            if (b[i - 1, j + 1] == '1')
+                                neighbour++;
+                            if (b[i, j + 1] == '1')
+                                neighbour++;
+                        }
+
+                        if(i == b.GetLength(1) && j == b.GetLength(0))
+                        {
+                            if (b[i, j - 1] == '1')
+                                neighbour++;
+                            if (b[i - 1, j - 1] == '1')
+                                neighbour++;
+                            if (b[i - 1, j] == '1')
+                                neighbour++;
+
+                        }
+
+                        if(j - 1 != 0 && j + 1 != b.GetLength(0) && i == b.GetLength(1))
+                        {
+                            if (b[i, j + 1] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][?]
+                            //[ ][ ][ ]
+                            if (b[i, j - 1] == '1')//PB LA
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[?][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i + 1, j] == '1')
+                                neighbour++;
+                            //[ ][?][ ]
+                            //[ ][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i - 1, j - 1] == '1')
+                                neighbour++;
+                            //[?][ ][ ]
+                            //[ ][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i - 1, j + 1] == '1')
+                                neighbour++;
+                            //[ ][ ][?]
+                            //[ ][1][ ]
+                            //[ ][ ][ ]
+                        }
+
+                        if (j - 1 != 0 && j + 1 != b.GetLength(0) && i == 0)
+                        {
+                            if (b[i, j + 1] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][?]
+                            //[ ][ ][ ]
+                            if (b[i, j - 1] == '1')//PB LA
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[?][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i - 1, j] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][ ]
+                            //[ ][?][ ]
+                            if (b[i + 1, j - 1] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][ ]
+                            //[?][ ][ ]
+                            if (b[i + 1, j + 1] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][ ]
+                            //[ ][ ][?]
+                        }
+
+                        if(i - 1 != 0 && i + 1 != b.GetLength(0) && j == b.GetLength(1))
+                        {
+                            if (b[i, j - 1] == '1')//PB LA
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[?][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i + 1, j] == '1')
+                                neighbour++;
+                            //[ ][?][ ]
+                            //[ ][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i - 1, j] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][ ]
+                            //[ ][?][ ]
+                            if (b[i - 1, j - 1] == '1')
+                                neighbour++;
+                            //[?][ ][ ]
+                            //[ ][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i + 1, j - 1] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][ ]
+                            //[?][ ][ ]
+
+                        }
+
+                        if (i - 1 != 0 && i + 1 != b.GetLength(0) && j == 0)
+                        {
+                            if (b[i + 1, j] == '1')
+                                neighbour++;
+                            //[ ][?][ ]
+                            //[ ][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i - 1, j] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][ ]
+                            //[ ][?][ ]
+                            if (b[i + 1, j + 1] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][ ]
+                            //[ ][ ][?]
+                            if (b[i - 1, j + 1] == '1')
+                                neighbour++;
+                            //[ ][ ][?]
+                            //[ ][1][ ]
+                            //[ ][ ][ ]
+                            if (b[i, j + 1] == '1')
+                                neighbour++;
+                            //[ ][ ][ ]
+                            //[ ][1][?]
+                            //[ ][ ][ ]
+                        }
+
+                    }
 
                     if (neighbour > 3 || neighbour < 2)//3 eme regle
                     {
