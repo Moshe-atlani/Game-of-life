a924ee102a6861db544b9f0d4027a02b3fe2bef0
Moshe-atlani <atlani.moshe@gmail.com>
2020-03-03 18:44:17 +0200

    I Finich BHM !!!

--- GOL2.0/GOL2.0/Program.cs
+++ GOL2.0/GOL2.0/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GOL2._0
@@ -13,9 +14,15 @@ namespace GOL2._0
             Board board = new Board();
             ConsoleGridShower shower = new ConsoleGridShower();
 
-            shower.show(board);
-            board.update();//je pense que dans cette peoula il faut touche le coeur de chaque cell
-            //shower.show(board);
+            //TimeSpan ts = new TimeSpan(0, 0, 1);
+            for (int i = 0; i < 300; i++)
+            {
+                shower.show(board);
+                board.update();//je pense que dans cette peoula il faut touche le coeur de chaque cell
+                               //shower.show(board);
+                Thread.Sleep(300);
+                Console.Clear();
+            }
         }
     }
 }
